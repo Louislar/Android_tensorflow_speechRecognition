9e71210c98914fbd1f8074f26f0aabfadc4d5f22
a8945267 <a8945267@example.com>
2019-05-17 17:15:12 +0800

    fix some .py file

--- tensorflow_reBoost/check_tensorflow_layer_name.py
+++ tensorflow_reBoost/check_tensorflow_layer_name.py
@@ -5,7 +5,7 @@
 
 import tensorflow as tf
 gf = tf.GraphDef()   
-m_file = open('my_frozen_graph.pb','rb')
+m_file = open('keras_to_tensorflowlite_success/keras_to_tensorflow/checkpoint/minst_trained_model.pb','rb')
 gf.ParseFromString(m_file.read())
 
 with open('somefile.txt', 'a') as the_file:
