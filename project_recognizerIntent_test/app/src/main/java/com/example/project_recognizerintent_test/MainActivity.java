045eea3c0bd64e1d2a1baf07ef16404d9765c1ac
a8945267 <a8945267@example.com>
2019-05-19 22:16:24 +0800

    新增簡易google內建 API專案，利用recognizer intent製作

---
+++ project_recognizerIntent_test/app/src/main/java/com/example/project_recognizerintent_test/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.project_recognizerintent_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.speech.RecognizerIntent;
+import android.content.Intent;
+import android.widget.TextView;
+import java.util.ArrayList;
+import 	android.content.ActivityNotFoundException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView playImg;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        playImg = findViewById(R.id.imageView);
+        textView = findViewById(R.id.textView);
+
+        playImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "請說～");
+                try{
+                    startActivityForResult(intent,200);
+                }catch (ActivityNotFoundException a){
+//                    Toast.makeText(getApplicationContext(),"Intent problem", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 200){
+            if(resultCode == RESULT_OK && data != null){
+                ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                textView.setText(result.get(0));
+            }
+        }
+    }
+}
